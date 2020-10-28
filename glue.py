de44c23e515b4cdd9ad66b1ec4ff96e8f26237ac
Vlad Mosko <totsamyzed@protonmail.com>
2020-10-28 21:33:03 +0300

    update gallery

--- glue.py
+++ glue.py
@@ -1,9 +1,16 @@
 import os
 import json
 
+section_names = [
+	'featured',
+	'social',
+	'crypto',
+	'currencies',
+	'tools'
+]
+
 sections = []
-for directory in os.scandir('sections/'):
-	section_name = directory.name
+for section_name in section_names:
 	with open('sections/{}/{}.json'.format(section_name, section_name)) as section_file: 
 		section = json.load(section_file)
 
@@ -11,9 +18,11 @@ for directory in os.scandir('sections/'):
 	for item in os.scandir('sections/{}'.format(section_name)):
 		if not item.name.endswith('.json'):
 			continue
-		if item.name != '{}.json'.format(section_name):
-			with open('sections/{}/{}'.format(section_name, item.name)) as widget_file:
-				widgets.append(json.load(widget_file))
+		if item.name == '{}.json'.format(section_name):
+			continue
+			
+		with open('sections/{}/{}'.format(section_name, item.name)) as widget_file:
+			widgets.append(json.load(widget_file))
 
 	section['widgets'] = widgets
 	sections.append(section)
